6d1204d59a95df2c290b316cfe2b8ae4e82459d4
Jacob Walser <jwalser90@gmail.com>
2017-11-14 21:30:10 -0500

    Use v4l2 controls

--- scripts/start_video.sh
+++ scripts/start_video.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-if lsusb | grep 05a3:9422; then
-  echo "USB Cam"
-  screen -dm -S video $HOME/companion/scripts/start_usbvid.sh
+
+# Load Pi camera v4l2 driver
+sudo modprobe bcm2835-v4l2
+
+if [ -z "$4" ]; then
+
+  echo 'No device specification'
+  if lsusb | grep 05a3:9422; then
+    echo "USB Cam"
+    DEVICE="/dev/video1"
+  else
+    echo "Raspi Cam"
+    DEVICE="/dev/video0"
+  fi
+
 else
-  echo "Raspi Cam"
-  DIR=$(dirname "$0")
-  camOptions=$(cat /home/pi/rpicamera.param)
-  gstOptions=$(cat /home/pi/gstreamer.param)
-  screen -dm -S video $HOME/companion/scripts/start_raspivid.sh "$camOptions" "$gstOptions"
+  DEVICE=$4
 fi
+
+gstOptions=$(tr '\n' ' ' < /home/pi/gstreamer2.param)
+
+echo "starting device $DEVICE with width $1 height $2 framerate $3 options $gstOptions"
+
+screen -X -S video quit
+screen -dm -S video bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -ev v4l2src device=$DEVICE do-timestamp=true ! video/x-h264, width=$1, height=$2, framerate=$3/1 $gstOptions"
