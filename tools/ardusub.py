2f11a8211284b8b92d6ec8b49cfca0ade6a59911
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-14 15:20:37 -0300

    ardusub: Move from tab to four spaces
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tools/ardusub.py
+++ tools/ardusub.py
@@ -8,22 +8,22 @@ import sys
 master = mavutil.mavlink_connection('udpout:localhost:9000')
 
 for x in range(3):
-	#Send request for autopilot version
-	master.mav.autopilot_version_request_send(
-		master.target_system,
-		master.target_component
-	)
-	msg = master.recv_match(type='AUTOPILOT_VERSION')
+    #Send request for autopilot version
+    master.mav.autopilot_version_request_send(
+        master.target_system,
+        master.target_component
+    )
+    msg = master.recv_match(type='AUTOPILOT_VERSION')
 
-	#Check if version information was obtained
-	#Print version
-	if msg:
-		version_d = msg.to_dict()
-		version_hex = hex(version_d["flight_sw_version"])
-		print ('.'.join(version for version in version_hex[2:-2:2]))
-		sys.exit(0)
+    #Check if version information was obtained
+    #Print version
+    if msg:
+        version_d = msg.to_dict()
+        version_hex = hex(version_d["flight_sw_version"])
+        print ('.'.join(version for version in version_hex[2:-2:2]))
+        sys.exit(0)
 
-	time.sleep(1)
+    time.sleep(1)
 
 #Exit the script with an error code after 3 attempts
 
